fc37c057fe17573c6525ccb0a0480ce96fa6d264
alexandreabade <alexandreabade@5a50b8e4-c46e-494c-8199-665086288703>
2017-05-17 19:55:03 +0000

    git-svn-id: https://svn.code.sf.net/p/rest-dataware-componentes/dataware@82 5a50b8e4-c46e-494c-8199-665086288703

--- URestPoolerDBMethod.pas
+++ URestPoolerDBMethod.pas
@@ -1,3 +1,12 @@
+{
+ Esse pacote de Componentes foi desenhado com o Objetivo de ajudar as pessoas a desenvolverem
+com WebServices REST o mais próximo possível do desenvolvimento local DB, com componentes de
+fácil configuração para que todos tenham acesso as maravilhas dos WebServices REST/JSON DataSnap.
+
+Desenvolvedor Principal : Gilberto Rocha da Silva (XyberX)
+Unit desenvolvida por   : Alexandre Abade
+Empresa : XyberPower Desenvolvimento
+}
 unit URestPoolerDBMethod;
 
 interface
