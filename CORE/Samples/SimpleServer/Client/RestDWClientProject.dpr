9e7b6269ad44eafa8a587e451f71e21011d28dc9
xybersportgames <xybersportgames@5a50b8e4-c46e-494c-8199-665086288703>
2017-08-24 01:47:22 +0000

    Remoção do madException
    git-svn-id: https://svn.code.sf.net/p/rest-dataware-componentes/dataware@387 5a50b8e4-c46e-494c-8199-665086288703

--- CORE/Samples/SimpleServer/Client/RestDWClientProject.dpr
+++ CORE/Samples/SimpleServer/Client/RestDWClientProject.dpr
@@ -2,11 +2,6 @@ program RestDWClientProject;
 {$APPTYPE GUI}
 
 uses
-  madExcept,
-  madLinkDisAsm,
-  madListHardware,
-  madListProcesses,
-  madListModules,
   Vcl.Forms,
   formMain in 'formMain.pas' {Form2},
   uDmClientDW in 'uDmClientDW.pas' {DataModule2: TDataModule};
